ed5f8b13b38a51e9daf617315d3c51f72c8cd331
Nikithamulagundla <mulagundlanikithareddy@gmail.com>
2023-04-02 21:31:08 +0530

    Merge remote-tracking branch 'origin/feature/Main' into feature/Main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!! Good Mrng");
         System.out.println("Hello India");
+        System.out.println("Nikii sravya best friends");
     }
 }
